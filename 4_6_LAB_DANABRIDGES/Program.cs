9db3ca619685121430eb6077ffaddeaeab08e2bb
enocpi <61128286+enocpi@users.noreply.github.com>
2020-02-27 09:03:00 -0600

    Add files via upload

---
+++ 4_6_LAB_DANABRIDGES/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+namespace _4_6_LAB_DANABRIDGES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday","sunday" };
+            string[] task = new string[7];
+            
+             
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine("type new task for" + " " + days[i]);
+                task[i] = days[i] + Console.ReadLine();
+
+                
+            };
+
+            for (int i = 0; i < task.Length; i++)
+            {
+                Console.WriteLine(task[i]);
+            };
+            
+
+           
+
+            
+            
+
+
+            
+                
+           
+
+          
+       
+
+          
+
+
+
+          
+           
+          
+            
+
+        }
+    }
+}
